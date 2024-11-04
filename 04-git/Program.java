532467f6e8f44117d56f868b2fe4ab994e0f1fcd
vasil <vasilka228228@yandex.by>
2024-11-04 02:04:43 +0100

    nowa zmiana 3 plik java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,8 @@
+//komentarzz
 public class Helloo {
     public static void main(String[] args){
         System.out.print("Hello World!");
         //zmiana 1
-        //zmiana 2
+        //zmiana 23
     }
 }
