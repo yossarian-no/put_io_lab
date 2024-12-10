8d0c0f3c7495bbc7c79c70fb6eae4d37aa2a4360
yossarian-no <145450951+yossarian-no@users.noreply.github.com>
2024-12-10 12:46:12 +0100

    Update FailureOrErrorTest.java

--- 08-junit/FailureOrErrorTest.java
+++ 08-junit/FailureOrErrorTest.java
@@ -1,1 +1,27 @@
+package put.io.testing.junit;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FailureOrErrorTest {
+
+    @Test
+    void test1() {
+        assertTrue(1 == 2);
+    }
+
+    @Test
+    void test2() {
+        Integer.parseInt("jeden");
+    }
+
+    @Test
+    void test3() {
+        try {
+            assertTrue(2 == 3);
+        } catch(Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
