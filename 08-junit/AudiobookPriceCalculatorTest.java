b3edc74bffa10c848d91ae057ce6f5839980c856
yossarian-no <145450951+yossarian-no@users.noreply.github.com>
2024-12-10 13:47:51 +0100

    Update AudiobookPriceCalculatorTest.java

--- 08-junit/AudiobookPriceCalculatorTest.java
+++ 08-junit/AudiobookPriceCalculatorTest.java
@@ -1,1 +1,42 @@
+package put.io.testing.audiobooks;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AudiobookPriceCalculatorTest {
+
+    private AudiobookPriceCalculator audiobookcalculator;
+    private Audiobook audiobook;
+
+    @BeforeEach
+    void setUp() {
+        audiobookcalculator = new AudiobookPriceCalculator();
+        audiobook = new Audiobook("TEST", 1000);
+    }
+
+    @Test
+    void test_calculateStandard() {
+        Customer Standard = new Customer("Standard", Customer.LoyaltyLevel.STANDARD, false);
+        assertEquals(audiobookcalculator.calculate(Standard, audiobook), 1000);
+    }
+
+    @Test
+    void test_calculateSilver() {
+        Customer Silver = new Customer("Silver", Customer.LoyaltyLevel.SILVER, false);
+        assertEquals(audiobookcalculator.calculate(Silver, audiobook), 0.9 * 1000);
+    }
+
+    @Test
+    void test_calculateGold() {
+        Customer Gold = new Customer("Gold", Customer.LoyaltyLevel.GOLD, false);
+        assertEquals(audiobookcalculator.calculate(Gold, audiobook), 0.8 * 1000);
+    }
+
+    @Test
+    void test_calculateSubscriber() {
+        Customer Subscriber = new Customer("Subscriber", Customer.LoyaltyLevel.STANDARD, true);
+        assertEquals(audiobookcalculator.calculate(Subscriber, audiobook), 0);
+    }
+}
